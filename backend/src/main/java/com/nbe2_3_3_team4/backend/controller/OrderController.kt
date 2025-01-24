20c072d3e3c960ad62974d025348445f7ed0e64d
devPixel <oasis23ckr@gmail.com>
2025-01-24 19:21:38 +0900

    feat : OrderController 추가

---
+++ backend/src/main/java/com/nbe2_3_3_team4/backend/controller/OrderController.kt
@@ -0,0 +1,74 @@
+package com.nbe2_3_3_team4.backend.controller
+
+import com.nbe2_3_3_team4.backend.domain.order.dto.OrderRequest
+import com.nbe2_3_3_team4.backend.domain.order.dto.OrderResponse
+import com.nbe2_3_3_team4.backend.domain.order.service.OrderService
+import com.nbe2_3_3_team4.backend.global.response.ApiResponse
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.boot.autoconfigure.security.SecurityProperties
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.*
+
+
+@RestController
+@RequestMapping("/api/orders")
+@Tag(name = "Order API", description = "주차권 주문 관련 API")
+class OrderController(
+    private val orderService: OrderService
+) {
+
+    @Operation(summary = "주차권 주문 요청 API", description = "주차권 구매를 요청합니다.")
+    @ApiResponses(io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "성공"))
+    @PostMapping("/tickets")
+    fun createOrder(
+        @RequestBody dto: OrderRequest.CreateOrder,
+        @AuthenticationPrincipal user: SecurityProperties.User
+    ): ResponseEntity<ApiResponse<OrderResponse.CreateOrder>> {
+        val response = orderService.createOrder(dto, user.name)
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.createSuccess(response))
+    }
+
+    @Operation(summary = "주차권 주문 결제 성공 API", description = "주차권 결제 성공 시 상태 변경합니다")
+    @ApiResponses(io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "성공"))
+    @PutMapping("/{payId}/success")
+    fun completePay(@PathVariable payId: String): ResponseEntity<ApiResponse<String>> {
+        val response = orderService.completePay(payId)
+        return ResponseEntity.ok(ApiResponse.createSuccess(response))
+    }
+
+    @Operation(summary = "주차권 구매 기록 상세 조회 API", description = "주차권 구매 세부 기록을 조회합니다.")
+    @ApiResponses(io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "성공"))
+    @GetMapping("/{orderId}")
+    fun getOrder(@PathVariable orderId: String): ResponseEntity<ApiResponse<OrderResponse>> {
+        val response = orderService.getOrder(orderId)
+        return ResponseEntity.ok(ApiResponse.createSuccess(response))
+    }
+
+    @Operation(summary = "주차권 취소 요청 API", description = "주차권 취소를 요청합니다.")
+    @ApiResponses(io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "성공"))
+    @PutMapping("/{orderId}/cancel")
+    fun cancelTicket(
+        @AuthenticationPrincipal user: SecurityProperties.User,
+        @PathVariable orderId: String
+    ): ResponseEntity<ApiResponse<String>> {
+        val response = orderService.cancelTicket(user.name, orderId)
+        return ResponseEntity.ok(ApiResponse.createSuccess(response))
+    }
+
+    @DeleteMapping("/{payId}")
+    fun deleteOrder(@PathVariable payId: String): ResponseEntity<ApiResponse<Void?>> {
+        return ResponseEntity.ok(ApiResponse.createSuccess(orderService.deleteOrder(payId)))
+    }
+
+    @Operation(summary = "주차권 구매 기록 조회 API", description = "주차권 구매 기록을 조회합니다.")
+    @ApiResponses(io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "성공"))
+    @GetMapping("/order-history")
+    fun getOrderHistory(@AuthenticationPrincipal user: SecurityProperties.User): ResponseEntity<ApiResponse<List<OrderResponse.GetOrderHistory>>> {
+        val response = orderService.getOrderHistory(user.name)
+        return ResponseEntity.ok(ApiResponse.createSuccess(response))
+    }
+}
