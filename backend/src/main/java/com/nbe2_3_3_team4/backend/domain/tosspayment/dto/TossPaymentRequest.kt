ebb7e638311056872f03af6cbeba24c9c985436b
DoHyun <citycozyboy@gmail.com>
2025-01-31 10:37:19 +0900

    FIX. allOpen 적용 및 일부 수정
    + 프론트 코드 추가

--- backend/src/main/java/com/nbe2_3_3_team4/backend/domain/tosspayment/dto/TossPaymentRequest.kt
+++ backend/src/main/java/com/nbe2_3_3_team4/backend/domain/tosspayment/dto/TossPaymentRequest.kt
@@ -1,12 +1,12 @@
 package com.nbe2_3_3_team4.backend.domain.tosspayment.dto
 
 class TossPaymentRequest {
-    class TempAmountSession(
+    data class TempAmountSession(
         val orderId: String,
         val amount: String
     )
 
-    class PaymentConfirmation(
+    data class PaymentConfirmation(
         val orderId: String,
         val paymentKey: String,
         val amount: String
